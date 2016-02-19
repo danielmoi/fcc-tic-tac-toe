979d55885d91df0b256696f916b1dc04e98905ca
danielmoi <daniel@ampersandmoi.com>
2016-02-20 10:09:55 +1100

    Game stops appropriately

--- src/js/app.js
+++ src/js/app.js
@@ -3,13 +3,13 @@ function echo(obj) {
 }
 window.echo = console.log.bind(console);
 
-var arrBoard = [],
+var arrBoard = ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
   turn,
-    index,
-    playerIcon,
-    computerIcon,
-    iconCross = '<i class="fa fa-times"></i>',
-    iconCircle = '<i class="fa fa-circle-o"></i>';
+  index,
+  playerIcon,
+  computerIcon,
+  iconCross = '<i class="fa fa-times"></i>',
+  iconCircle = '<i class="fa fa-circle-o"></i>';
 
 $('.square').click(function () {
   var self = this;
@@ -22,60 +22,81 @@ $('.square').click(function () {
 var arrNames = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
 
 function playerMove(id) {
-  
+
   turn = 'playerTurn';
-  
+
   // get index from arrNames
   index = arrNames.indexOf(id);
-  
+
   // check square is occupied by player
   if (arrBoard[index] === 'player') {
     echo('you\'re already here');
     return;
   }
-  
+
   // check if square is occupied by computer
   if (arrBoard[index] === 'computer') {
     echo('computer here');
     return;
   }
-  
+
   // write to arrBoard
   arrBoard[index] = 'player';
-  
-  
+
   // draw to screen
-  $('#'+id).append(iconCross);
-  
-  
+  echo('player move: ' + index);
+  echo('arrBoard[index]: ' + arrBoard[index]);
+
+  $('#' + id).append(iconCross);
+
   echo(arrBoard);
-  
+
   // now allow computer to move
   turn = 'computerTurn';
   return;
 }
 
-       
+
 
 
 function resetBoard() {
-  arrBoard = [];
+  arrBoard = ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'];
   $('.square').empty();
   echo(arrBoard);
 }
 
+function checkBoard() {
+  if (arrBoard.indexOf('empty') === -1) {
+    echo(arrBoard.length);
+    echo('game over');
+    return false;
+  }
+  return true;
+}
+
 function computerMove() {
   if (turn === 'computerTurn') {
-    var random = Math.floor(Math.random() * 9);
-    echo('computer move: ' + random);
-    
-    // convert move to word
-    var index = arrNames[random];
-    echo(index);
-    
-    $('#'+index).append(iconCircle);
-    
-    turn = 'playerTurn';
+    if (checkBoard()) {
+      do {
+        var random = Math.floor(Math.random() * 9);
+        echo('random: ' + random);
+
+        // convert move to word
+        echo('computer index: ' + random);
+        echo('arrBoard[index]: ' + arrBoard[random]);
+
+      }
+      while (arrBoard[random] === 'player' || arrBoard[random] === 'computer');
+
+
+      arrBoard[random] = 'computer';
+      echo('computer move: ' + random);
+
+      var indexWord = arrNames[random];
+      $('#' + indexWord).append(iconCircle);
+
+      turn = 'playerTurn';
+    }
   }
   return;
 }
