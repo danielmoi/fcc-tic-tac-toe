576c641fcce05e3ff72c429a3dde700e06c3b603
danielmoi <daniel@ampersandmoi.com>
2016-02-19 15:38:58 +1100

    Basic framework. Problems with scope

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,53 @@
-var positions = [];
+function echo(obj) {
+  console.log(obj);
+}
+window.echo = console.log.bind(console);
 
-$('.square').click(function() {
-  console.dir(this);
-  $(this).append('<i class="fa fa-times"></i>');
-  positions.push(this.id);
-  console.log(positions);
+var arrBoard = [],
+  turn;
+
+$('.square').click(function () {
+  var self = this;
+  var obj = {};
+  obj.index = '';
+  $(self).append('<i class="fa fa-times"></i>');
+  checkSquare(self.id);
+  echo('line 14: obj.index: ' + obj.index);
+  playerMove(index);
+  computerMove();
+
 });
 
-var track = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
+var arrNames = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
+
+function checkSquare(id) {
+  index = arrNames.indexOf(id);
+  obj.index = index;
+  echo('line 23: index: ' + index);
+}
+       
+       
+       
+function playerMove(index) {
+  arrBoard[index] = 'player';
+  echo('line 30: '+ arrBoard);
+}
+
+function resetBoard() {
+  arrBoard = [];
+  $('.square').empty();
+  echo(arrBoard);
+}
+
+function computerMove() {
+  var random = Math.floor(Math.random() * 9);
+  echo(random);
+}
+
+$('.resetBoard').click(function () {
+  resetBoard();
+});
+
+$('.computer').click(function () {
+  computerMove();
+});
